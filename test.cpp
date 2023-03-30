009a16bb07437fa5c51fa59ec9616aee2b49b7c7
Vinay jangam <84473308+VinayJango@users.noreply.github.com>
2023-03-30 17:07:01 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    //dec all info below strings and etc
+    string Full_name, Bank_name, Account_number, datet, dater;
+    int user_input;
+    cout << "---------------------------------------------------------------------------------------------" <<endl;
+    cout << "GHANASHALI ENTERPRISES SYSv0.1 --- {DEVELOPER-Vinay_jangam}" <<endl;
+    cout << "---------------------------------------------------------------------------------------------" <<endl;
+    cout << "Enter Name: ";
+    cin >> Full_name;
+    cout << "Enter Bank_name:  ";
+    cin >> Bank_name;
+    cout << "Enter Account_number:  ";
+    cin >> Account_number;
+    cout << "Enter DATE OF TRANSFER:  ";
+    cin >> datet;
+    cout << "ENTER DATE OF RECIVING:  ";
+    cin >> dater;
+    cout << "-------------------------- " <<endl;
+    cout << "ENTER {1} FOR VIEWING INFO" <<endl; 
+    cout << "ENTER {2} FOR QUITTING" <<endl;
+    cout << "-------------------------- " <<endl;
+//getting the user input here 
+    cin >> user_input;
+  
+    if (user_input == 1)
+    {
+        cout << "Full name is " + Full_name <<endl;
+        cout << "Bank name is " + Bank_name <<endl;
+        cout << "Account number is " + Account_number <<endl;
+        cout << "date of transfer is " + datet <<endl;
+        cout << "date of reciving is " + dater <<endl;
+
+    }
+    else if (user_input == 2)
+    {
+        cout << "QUITED BYE ~" <<endl;
+    }
+    else
+    {
+        cout << "INVALID OPTION~!" <<endl;
+    }
+    system("pause");
+    return 0;
+}
